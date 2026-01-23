288ff26ca2604359b8913e661ed45f0d1b7b8303
widarbasaridua-ai <widarbasaridua@gmail.com>
2026-01-23 13:00:23 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,18 @@
-const CACHE_NAME = 'jimpitan-digital-v3';
+const CACHE_NAME = 'jimpitan-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/favicon.ico'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.map(key => 
-        key !== CACHE_NAME ? caches.delete(key) : null
-      ))
-    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.url.includes('script.google.com')) {
-    return; // Don't cache Google Script
-  }
-  
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
